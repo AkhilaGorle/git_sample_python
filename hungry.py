7b0147c0625d85616eb77e41167bf1b38e7aa1ad
Akhila Gorle <akhila.gorle@treon.fi>
2023-03-10 14:04:35 +0200

    fruits

--- hungry.py
+++ hungry.py
@@ -2,6 +2,8 @@ print("Welcome to second program")
 hungry=input("Are you hungry?")
 if hungry=="yes":
     print("Eat vegetables")
+    print("Eat fruits")
+    print("Eat nuts")
 else:
     print("Do homework")
  
