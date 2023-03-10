0424e46339d5497169f96819203d66ce288da957
Akhila Gorle <akhila.gorle@treon.fi>
2023-03-10 14:02:23 +0200

    Revert "hungry"
    This reverts commit 1b4202f373c151da11865cf6ae9999ba5f86a870.

--- hungry.py
+++ hungry.py
@@ -2,8 +2,6 @@ print("Welcome to second program")
 hungry=input("Are you hungry?")
 if hungry=="yes":
     print("Eat vegetables")
-    print("Eat fruits")
-    print("Eat chips")
 else:
     print("Do homework")
  
